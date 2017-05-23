be2f01673f58457aebe72ea5ed0a394c7beffed2
dbpeetz <d.peetz516@gmail.com>
2017-05-23 15:48:42 -0500

    Added Service Layer/Methods

---
+++ DLDShoppingList.Models/ListItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLDShoppingList.Models
+{
+    class ListItem
+    {
+        public int ShoppingListItemID { get; set; }
+
+        public string Contents { get; set; }
+
+        public string Note { get; set; }
+
+        [UIHint("Starred")]
+        public bool IsChecked { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public override string ToString() => $"[{ShoppingListItemID}] {Contents}";
+
+    }
+}
---
+++ DLDShoppingList.Service/ListService.cs
@@ -0,0 +1,43 @@
+﻿using DLDShoppingList.Data;
+using DLDShoppingList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLDShoppingList.Service
+{
+    class ListService
+    {
+        public bool CreateList(ListCreate model)
+        {
+            var entity = new ShoppingList
+            {
+                Name = model.Name,
+                Color = model.Color,
+                CreatedUtc = DateTimeOffset.UtcNow
+            };
+
+            using (var ctx = new ApplicationDbContext()) 
+            {
+                ctx.ShoppingList.Add(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteItem(int itemId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.ShoppingListItem.Single
+                    (e => e.ShoppingListItemID == itemId);
+
+                ctx.ShoppingListItem.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
