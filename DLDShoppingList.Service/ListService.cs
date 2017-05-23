a09a2221a03b24f67447d4064ae5b052e15069ad
LDecker <Decker702@yahoo.com>
2017-05-23 15:09:36 -0400

    Work on Delete

---
+++ DLDShoppingList.Service/ListService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLDShoppingList.Service
+{
+    public class ListService
+    {
+  
+
+        public bool DeleteshoppingList(int ItemId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                      ctx
+                      .Item
+                      .Single(e => e.ItemID == ItemId);
+
+                ctx.Notes.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+
+            }
+        }
+
+    }
+}
